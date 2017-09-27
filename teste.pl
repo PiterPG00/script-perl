4f5170ff65ae488d3bd6cfc8a37b00109191f652
PiterPG00 <31753341+PiterPG00@users.noreply.github.com>
2017-09-26 23:11:31 -0300

    Add files via upload

---
+++ teste.pl
@@ -0,0 +1,424 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Term::ANSIColor;
+
+print color("blue"),"\nSCRIPT EM BETA!\n",color("reset");
+
+ print color("magenta"),"\nQual cor você deseja ultilizar? ",color("reset");
+
+
+print color("green"),"\n                           CORES DISPONIVEIS!\n";
+print color("blue"),"| AZUL ";
+print color("magenta"),"| MAGENTA ";
+print color("green"),"| VERDE ";
+print color("red"),"| VERMELHO ";
+print color("yellow"),"| AMARELO ";
+print color("white"),"| BRANCO ";
+print color("cyan"),"| CYANO ";
+print color("white"),"| PRETO |\n";
+print color("cyan"),"                     ESCREVA COM LETRAS MINUSCULAS!\n";
+
+print color("magenta"),"COR DESEJADA: ",color("reset");
+
+chomp ( my $cor = <> );
+
+
+
+
+
+if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+
+
+
+print "\nQual seu user? ";
+
+
+
+
+print color("red");
+
+if ( $cor eq "vermelho") {
+	print color("cyan")
+}
+
+chomp ( my $user = <STDIN> );
+
+
+if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+
+
+
+
+print "\nOla";
+
+print color("red");
+
+if ( $cor eq "vermelho") {
+	print color("cyan")
+}
+
+print " $user ";
+
+
+if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+
+
+print ",Bem Vindo!\n";
+
+print "\nQual seu Sistema Operacional? ";
+
+print color("Cyan");
+
+if ( $cor eq "cyano") {
+	print color("red")
+}
+
+chomp ( my $sistema = <STDIN> );
+
+
+if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+
+
+print "\nSeu Sistema Operacional"; 
+
+print color("Cyan");
+
+if ( $cor eq "cyano") {
+	print color("red")
+}
+
+print " $sistema "; 
+
+
+if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+
+
+print ", Você está Gostando dele? ";
+
+print color("red");
+
+if ( $cor eq "vermelho") {
+	print color("cyan")
+}
+
+chomp ( my $resposta = <STDIN> );
+
+
+if ($resposta eq "sim") {
+	
+
+	if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+
+	print "\nPor que você Gostou dele ?\n"
+}
+
+
+
+
+
+elsif ( $resposta eq "nao" ) {
+
+
+	if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+	print "\nPor que você não Gostou dele ?\n"
+
+
+}
+
+
+
+
+
+else {
+	print color("green"),"\nPor favor responda com sim ou nao !\n"
+}
+
+
+if ( $cor eq "azul") {
+	print color("blue");
+}
+
+
+if ( $cor eq "magenta" ) {
+ 	print color("magenta");
+ }
+ 	
+
+if ( $cor eq "verde" ) {
+ 	print color("green");
+ }
+
+ 
+if ( $cor eq "vermelho") {
+	print color("red")
+}
+
+if ( $cor eq "amarelo") {
+	print color("yellow")
+}
+
+if ( $cor eq "branco") {
+	print color("white")
+}
+
+if ( $cor eq "cyano") {
+	print color("cyan")
+}
+
+if ( $cor eq "preto") {
+	print color("black")
+}
+
+
+print "\n\n",color("reset");
+
+print "Ele ainda está em desenvolvimento!\n";
+print "Não terminei ele ainda , pois estou testando uma coisa nova pra mim adicionar\nnesse script!\n";
+print "\nColabora ae ;-;\nDiz que ficou dahora! :)\n";
+print "\nÉ NOIS!\n";
+
+print "\n\n";
+
+$ENV{'TZ'} = 'America/Sao_Paulo'; # Especifique o timezone aqui!
+my ($sec, $min, $hour) = localtime;
+printf("Você entrou as: %02d:%02d:%02d!\n", $hour, $min, $sec );
+
+print "\n\n";
+print "Criador : PiterPG\n\n"; 
+
+
+print color("reset");
+exit;
